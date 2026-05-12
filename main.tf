10792475d9dfbe3371b4a240342e4a91005f651e
swilts000 <sharif_wiltshire@cable.comcast.com>
2026-05-12 11:21:13 -0400

    ok

--- main.tf
+++ main.tf
@@ -1,17 +1,23 @@
+variable "ecr_name" {
+  description = "Name of the ECR repository"
+  type        = string
+  default     = "repoforgithub"
+}
+
 provider "aws" {
   region = "us-east-2"
 }
 
 resource "aws_ecr_repository" "repoGit" {
-  name                 = "repoforgithub"
-  image_tag_mutability = "IMMUTABLE" # Prevents overwriting tags
+  name                 = var.ecr_name
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
-    scan_on_push = true # Automatically scans images for vulnerabilities
+    scan_on_push = true
   }
 
   encryption_configuration {
-    encryption_type = "AES256" # Default encryption
+    encryption_type = "AES256"
   }
 
   tags = {
